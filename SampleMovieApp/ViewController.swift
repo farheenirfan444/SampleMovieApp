e6ce7d10696028a5c1de60f9c9811bf254e5ed0b
Farheen Irfan <farheen.irfan@dubizzlelabs.com>
2026-02-14 16:13:45 +0500

    Initial Commit

---
+++ SampleMovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleMovieApp
+//
+//  Created by Faheem Irfan on 14/02/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
